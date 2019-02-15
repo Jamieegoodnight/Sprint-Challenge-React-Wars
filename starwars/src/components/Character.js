7b469d31244792907c02ba13512c9f8d6d5aeab4
Jamie Goodnight <jamiegoodnight@Jamies-Air.attlocal.net>
2019-02-15 11:20:15 -0500

    Character template created. CharacterList component created.

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Character = props => {
-    
+    <div>
+        <img src={props.element.url} alt="Image of the character."/>
+        <ul>
+            <li>Name: {props.element.name}</li>
+        </ul>
+    </div>
 }
 
 export default Character
